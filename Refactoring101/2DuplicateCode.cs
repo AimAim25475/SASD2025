98a83be7db9d34aa03e453004f02d7ff40ae193b
Ken <tinnapop.tn@gmail.com>
2026-01-23 16:42:56 +0700

    create Refactor101

---
+++ Refactoring101/2DuplicateCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring101;
+
+// 2
+public class DuplicateCodeGood1
+{
+    string name = "Jimmy";
+    public void Run()
+    {
+        PrintName("Owner");
+        PrintBody();
+        PrintName("Signed");
+    }
+    private void PrintName(string label)
+    {
+        Console.WriteLine(label + " : " + name);
+    }
+    private void PrintBody()
+    {
+        Console.WriteLine("This is to certify that...");
+    }
+}
+
+
---
+++ Refactoring101/3ShotgunSurgeryEasy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+// 3
+namespace Refactoring101.ShortgunSurgeryEasy
+public class constants
+{
+    public const string Owner = "Johny Black";
+}
+public class ShotgunA
+{
+    public void PrintReportA()
+    {
+            Console.WriteLine("Owner : " + Constant.owner);
+    }
+}
+public class ShotgunB
+{
+        public void PrintReportB()
+        {
+            Console.WriteLine("Signer : " + Constant.owner);
+        }
+    }
+}
+
